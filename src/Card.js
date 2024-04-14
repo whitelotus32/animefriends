1865136ed841aa8cc8f954278e2e0ebe12029ddc
Nicholas Dixon <naseer2152@gmail.com>
2024-04-14 14:06:13 -0500

    Created the App file and made some syntax adjustments.

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,14 @@
-import React, {Fragment} from "react";
+import React, {Fragment} from 'react';
 
 const Card = (props) => {
     return (
-            <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-                <img alt='robots' src={`https://robohash.org/${props.id}?200x200/`}>
-                <div>
-                    <h2>{props.name}</h2>
-                    <p>{props.email}</p>
-                </div>
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img alt='robots' src={`https://robohash.org/${props.id}?200x200/`}>
+            <div>
+                <h2>{props.name}</h2>
+                <p>{props.email}</p>
             </div>
+        </div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { robots } from './robots';
 
-ReactDOM.render(
-            <CardList robots={robots}/>
-,document.getElementById('root'));
+ReactDOM.render(<App />,document.getElementById('root'));
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
