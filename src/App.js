c25cecda0e768cb848f64c9ac2791eedc608010f
Nicholas Dixon <naseer2152@gmail.com>
2024-04-14 14:22:35 -0500

    Created the SearchBox file.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
-import SearchBox from './SearchBox';
+import SearchBox from './';
 
 const App = () => {
     return (
-        <div>
+        <div className='tc'>
             <h1>RoboFriends</h1>
             <CardList robots={robots}/>
         </div>
