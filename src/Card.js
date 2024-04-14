5d735df1052f7e1eac168a4114de8551fd57aabe
Nicholas Dixon <naseer2152@gmail.com>
2024-04-14 09:17:44 -0500

    Added CSS styling to the div.

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Card = () => {
     return (
-        <div>
-            <img alt='robots' src='https://robohash.org/test?200x200'/>
+        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img alt='robots' src='https://robohash.org/3?200x200'/>
             <div>
                 <h2>Jane Doe</h2>
                 <p>jane.doe@gmail.com</p>
