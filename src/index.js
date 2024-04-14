0fd21b0066e241059c9135f071b38463a5c66df8
Nicholas Dixon <naseer2152@gmail.com>
2024-04-14 09:25:05 -0500

    Created the robots.js file with an array full of objects.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import { robots } from './robots';
 
 ReactDOM.render(
   <div>
