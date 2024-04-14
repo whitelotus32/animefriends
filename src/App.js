77e3ea89a139e098a83b897cc0945ca62ab331e5
Nicholas Dixon <naseer2152@gmail.com>
2024-04-13 21:19:01 -0500

    Corrected my code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-class App extends React.Component() {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
