46f66ca023534bd54869dcf42b13032de9e46242
Nicholas Dixon <naseer2152@gmail.com>
2024-04-15 13:16:54 -0500

    Debugged a couple errors.

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,9 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 
 const Card = (props) => {
     return (
         <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img alt='robots' src={`https://robohash.org/${props.id}?200x200/`}>
+            <img alt='robots' src={`https://robohash.org/${props.id}?200x200/`} />
             <div>
                 <h2>{props.name}</h2>
                 <p>{props.email}</p>
