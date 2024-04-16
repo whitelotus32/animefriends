820029ad59b0fd9b80b82cab22cb139bc79bca39
Nicholas Dixon <naseer2152@gmail.com>
2024-04-15 21:58:15 -0500

    Debugged issues

--- src/Scroll.js
+++ src/Scroll.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Scroll = () => {
+const Scroll = (props) => {
     return (
         <div style={{ overflowY: 'Scroll', border: '5px solid black', height: '800px' }}>
             {props.children};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
