a5cd5678f63175d3a531644e5b58e2543cedf4a6
Nicholas Dixon <naseer2152@gmail.com>
2024-04-16 18:07:42 -0500

    Dummy commit: Had to replace the original files.

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Scroll = (props) => {
-    return (
-        <div style={{ overflowY: 'Scroll', border: '5px solid black', height: '800px' }}>
-            {props.children};
-        </div>
-    );
+  return (
+    <div style={{ overflow: 'scroll', border: '5px solid black', height: '800px'}}>
+      {props.children}
+    </div>
+  );
 };
 
 export default Scroll;
--- src/index.js
+++ src/index.js
@@ -6,8 +6,11 @@ import App from './containers/App';
 import 'tachyons';
 // import registerServiceWorker from './registerServiceWorker';
 
+
+
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
+
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
